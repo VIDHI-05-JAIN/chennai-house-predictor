6b2be05be4650df6cc6ef43c9a0c08602973e6c5
VIDHI-05-JAIN <143104957+VIDHI-05-JAIN@users.noreply.github.com>
2026-06-10 10:23:00 +0530

    fix model path

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ st.set_page_config(
 # ==============================
 # 📦 LOAD MODEL & DATA
 # ==============================
-MODEL_PATH = "/Users/admin/Desktop/ML_Project/chennai-house-predictor/model/house_price_pipe.pkl"
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "../model/house_price_pipe.pkl")
 DATA_PATH = "data/train-chennai-sale.csv"
 
 if not os.path.exists(MODEL_PATH):
